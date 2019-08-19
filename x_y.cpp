5742d75b3e7b28ac3902d7c1e5fd2090629cbc13
Delton (John) Oliver <delton.oliver@outlook.com>
2019-08-19 17:39:27 -0500

    Add files via upload

---
+++ x_y.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+	int x, y, z;
+	x = 5;
+	std::cout << "x is " << x << "\n";
+
+	y = 7;
+	std::cout << "y is " << y << "\n";
+
+	std::cout << x << " + " << y << " = " << x + y << "\n";
+
+	return 0;
+}
